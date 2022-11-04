f7dff541f73ede18c10004cb3075ba4974bff9ec
santizabe <santizabe@gmail.com>
2022-11-04 15:25:33 -0500

    Update dashboard. Add and style grid

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NotesContainer from "../NotesComponents/NotesContainer";
+import NoteStatus from "../NotesComponents/NoteStatus";
 
 function Dashboard() {
   return (
     <>
       <NotesContainer />
+      <NoteStatus/>
     </>
   );
 }
