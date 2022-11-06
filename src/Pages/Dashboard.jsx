53d7680e90cf08f252a602f7653ca4d22257c7c2
santizabe <santizabe@gmail.com>
2022-11-05 21:40:56 -0500

    Setup dashboard private route

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import NotesContainer from "../NotesComponents/NotesContainer";
 import NoteStatus from "../NotesComponents/NoteStatus";
+import { useAuth } from "../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
 
 function Dashboard() {
+  const { currentUser } = useAuth();
+
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <>
       <NotesContainer />
-      <NoteStatus/>
+      <NoteStatus />
     </>
   );
 }
